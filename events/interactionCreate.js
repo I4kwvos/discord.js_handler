ea50314facedf46913daf6b73bd0eb4d948bcae2
Iakwvos <djn0506@gmail.com>
2022-07-01 00:02:46 +0300

    Update interactionCreate.js

--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -1,21 +1,13 @@
 const client = require(`../index.js`)
 
 client.on(`interactionCreate`, async interaction => {
-    if (interaction.isCommand()) {
-        const cmd = client.slashCommands.get(interaction.commandName) 
+    if (interaction.isCommand() || interaction.isContextMenu()) {
+        const cmd = client.slashCommands.get(interaction.commandName)
         if (!cmd) {
             interaction.deferReply({ ephemeral: true })
-            interaction.followUp({ content: "An error has occured!" }) 
+            interaction.followUp({ content: "An error has occured!" })
         }
 
-        cmd.run(client, interaction) 
-    } else if (interaction.isContextMenu()) {
-        const cmd = client.slashCommands.get(interaction.commandName) 
-        if (!cmd) {
-            interaction.deferReply({ ephemeral: true })
-            interaction.followUp({ content: "An error has occured!" }) 
-        }
-
-        cmd.run(client, interaction) 
+        cmd.run(client, interaction)
     }
 })
